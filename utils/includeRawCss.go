409d2b74760148de19ba84418265b5ec99490f04
mateusLWD <alves.mateus1050@gmail.com>
2026-04-01 16:49:40 +0100

    .

--- utils/includeRawCss.go
+++ utils/includeRawCss.go
@@ -7,12 +7,11 @@ import (
 )
 
 func IncludeRawCSSFile(path string) template.HTML {
-	// Just use the path directly.
-	// If you pass "components/nav_bar/style.css", it looks in the current folder.
 	content, err := os.ReadFile(path)
 	if err != nil {
 		log.Printf("Error reading CSS file %s: %v", path, err)
 		return ""
 	}
+
 	return template.HTML("<style>" + string(content) + "</style>")
 }
